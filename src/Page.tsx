f7438b4ae239f033d5b58a1ec25e41c9e1e1475d
Felix Koch <mail@felixkoch.de>
2021-09-16 09:41:10 +0200

    Menue

--- src/Page.tsx
+++ src/Page.tsx
@@ -1,18 +1,20 @@
 
 import { Avatar } from '@chakra-ui/avatar'
 import { Button } from '@chakra-ui/button'
-import { Box, Container, Divider, Flex, Heading, HStack, Link as ChakraLink, List, ListIcon, ListItem, SimpleGrid, Text, Wrap, WrapItem } from '@chakra-ui/layout'
+import { Box, Container, Divider, Flex, Heading, HStack, Link as ChakraLink, List, ListIcon, ListItem, SimpleGrid, Text, VStack, Wrap, WrapItem } from '@chakra-ui/layout'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { PhoneIcon, EmailIcon, CalendarIcon } from '@chakra-ui/icons'
+import { PhoneIcon, EmailIcon, CalendarIcon, HamburgerIcon } from '@chakra-ui/icons'
 import { FaLinkedin, FaGithub } from "react-icons/fa"
 import { Tag } from '@chakra-ui/tag'
-import { Icon } from "@chakra-ui/react"
+import { Icon, IconButton,useDisclosure } from "@chakra-ui/react"
 import Link from "next/link"
+import { Collapse } from "@chakra-ui/transition"
 
 export default function Page(props: any) {
 
   const { startPage } = props
+  const { isOpen, onToggle } = useDisclosure()
 
   return (
     <div >
@@ -22,38 +24,64 @@ export default function Page(props: any) {
         <link rel="icon" href="/web.jpg" />
       </Head>
       <header>
-        <Box bgColor="#ffd100" pt={startPage ? 5 : 4}  pb={startPage ? 5 : 4}>
+        <Box bgColor="#ffd100" pt={startPage ? 5 : 4} pb={startPage ? 5 : 4}>
           <Container maxW={1280} >
             <Flex justifyContent="space-between" alignItems="center">
               <Link href="/">
                 <Avatar name="Felix Koch" src="/web.jpg" size={startPage ? "lg" : "md"} />
-                
+
               </Link>
 
               <Wrap spacing={4}>
-                <WrapItem>
+                <WrapItem display={['none', 'none', 'none', 'block']}>
                   <a href="mailto:felix@tagungshotels.info" style={{ textDecoration: 'none' }}>
                     <Button leftIcon={<EmailIcon />} variant="outline" size="sm" borderColor="gray.800" borderWidth="3px">
                       felix@tagungshotels.info
                     </Button>
                   </a>
                 </WrapItem>
-                <WrapItem>
+                <WrapItem display={['none', 'none', 'none', 'block']}>
                   <a href="tel:004942669999999" style={{ textDecoration: 'none' }}>
                     <Button leftIcon={<PhoneIcon />} variant="outline" size="sm" borderColor="gray.800" borderWidth="3px">
                       +49 4266 999 999 9
                     </Button>
                   </a>
                 </WrapItem>
-                <WrapItem>
+                <WrapItem display={['none', 'none', 'none', 'block']}>
                   <a href="https://meetings.hubspot.com/felix137" style={{ textDecoration: 'none' }}>
                     <Button leftIcon={<CalendarIcon />} variant="outline" size="sm" borderColor="gray.800" borderWidth="3px">
                       Make an appointment
                     </Button>
                   </a>
                 </WrapItem>
+                <WrapItem display={['block', 'block', 'block', 'none']}>
+                  <IconButton onClick={onToggle} aria-label="Menue" icon={<HamburgerIcon />} variant="outline" size="sm" borderColor="gray.800" borderWidth="3px" />
+                </WrapItem>
               </Wrap>
             </Flex>
+            <Collapse in={isOpen} animateOpacity>
+              <VStack spacing={4} alignItems="start" mt={4}>
+
+                <a href="mailto:felix@tagungshotels.info" style={{ textDecoration: 'none' }}>
+                  <Button leftIcon={<EmailIcon />} variant="outline" size="sm" borderColor="gray.800" borderWidth="3px">
+                    felix@tagungshotels.info
+                  </Button>
+                </a>
+
+                <a href="tel:004942669999999" style={{ textDecoration: 'none' }}>
+                  <Button leftIcon={<PhoneIcon />} variant="outline" size="sm" borderColor="gray.800" borderWidth="3px">
+                    +49 4266 999 999 9
+                  </Button>
+                </a>
+
+                <a href="https://meetings.hubspot.com/felix137" style={{ textDecoration: 'none' }}>
+                  <Button leftIcon={<CalendarIcon />} variant="outline" size="sm" borderColor="gray.800" borderWidth="3px">
+                    Make an appointment
+                  </Button>
+                </a>
+
+              </VStack>
+            </Collapse>
 
           </Container>
         </Box>
